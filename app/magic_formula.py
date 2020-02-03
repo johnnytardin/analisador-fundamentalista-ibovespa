aab69dcdfce4636dc3e0ef356547486ecf4252fc
johnnytardin@gmail.com <johnny.tardin@m4u.com.br>
2020-02-03 12:03:04 -0300

    Novas métricas de score e base

--- app/magic_formula.py
+++ app/magic_formula.py
@@ -1,34 +1,41 @@
 import db
 
-ev_ebit = {}
-roic = {}
 
-magic_formula = {}
+def get_result():
+    ev_ebit, roic, magic_formula = {}, {}, {}
 
-count = 1
-for row in db.select_ev_ebit():
-    ev_ebit[row[0]] = count
-    count += 1
+    count = 0
+    for row in db.select_ev_ebit():
+        ev_ebit[row[0]] = count
+        count += 1
+
+    count = 0
+    for row in db.select_roic():
+        roic[row[0]] = count
+        count += 1
+
+    for code, score in ev_ebit.items():
+        magic_formula[code] = score + roic[code]
+
+    ordered = {
+        k: v
+        for k, v in sorted(
+            magic_formula.items(), key=lambda item: item[1], reverse=True
+        )
+    }
+    return ordered
 
 
-count = 1
-for row in db.select_roic():
-    roic[row[0]] = count
-    count += 1
+def main():
+    count = 0
+    magic_result = get_result()
+    for code, score in magic_result.items():
+        print(f"{count} - {code} - {score}")
+        count += 1
 
-for code, score in ev_ebit.items():
-    magic_formula[code] = score + roic[code]
+        if count == 30:
+            break
 
 
-ordered = {
-    k: v
-    for k, v in sorted(magic_formula.items(), key=lambda item: item[1], reverse=True)
-}
-
-count = 1
-for code, score in ordered.items():
-    print(f"{count} - {code} - {score}")
-    count += 1
-
-    if count == 31:
-        break
+if __name__ == "__main__":
+    main()
