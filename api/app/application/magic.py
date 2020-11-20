469bc053f07668f9d4f598d2a525a0584cad4eb9
Johnny Tardin <johnny.tardin@m4u.com.br>
2020-11-20 15:36:10 -0300

    Melhorias diversas

--- api/app/application/magic.py
+++ api/app/application/magic.py
@@ -105,7 +105,7 @@ def rank(estrategia, payload):
         if lucros.valida_empresa(code):
             # adiciona indicadores
             ind = financial.financial_get_indicators(code)
-            technical = db.consulta_detalhes("technical", code)
+            technical = db.consulta_detalhes("technical", code)[0]
 
             rank_validated.append(
                 [
