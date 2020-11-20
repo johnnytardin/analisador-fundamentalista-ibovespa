76ce9a5e4779aee0bfea50ceb4978d48785bc8a3
Johnny Tardin <johnny.tardin@m4u.com.br>
2020-11-20 12:01:13 -0300

    Melhorias diversas

--- api/app/application/technical.py
+++ api/app/application/technical.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_technical_indicators(code, interval="weekly"):
-    time.sleep(1.5)
+    #time.sleep(1.5)
 
     data = inv.technical_indicators(
         code, country="brazil", product_type="stock", interval=interval
