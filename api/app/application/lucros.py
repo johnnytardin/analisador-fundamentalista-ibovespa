5fd1c4371d68e00318cbdc2d23006bb843597cc4
Johnny Tardin <johnny.tardin@m4u.com.br>
2020-12-30 16:55:49 -0300

    Atualizando conf

--- api/app/application/lucros.py
+++ api/app/application/lucros.py
@@ -37,20 +37,21 @@ def valida_ultimos_lucros(lucros, ultimos_12m):
     # verifica se os lucros vem caindo
     lucros_desc, ctrl = 0, 0
     data_l = sorted(lucros.items(), key=lambda item: item[0], reverse=False)[-3:]
+    lucros = [v[1] for v in data_l]
+    lucros.sort(reverse=True)
 
     ultimo_lucro, ctrl = None, 0
-    for v in data_l.sort(reverse=True):
-        vlr = v[1]
+    for l in lucros:
         ctrl += 1
 
         # verifica se o lucro vem caindo
         if ultimo_lucro:
             # ex: se 2018 for menor que 2017 (gordura de x%)
-            if vlr < (ultimo_lucro * 0.8):
+            if l < (ultimo_lucro * 0.8):
                 lucros_desc += 1
-                ultimo_lucro = vlr
+                ultimo_lucro = l
         else:
-            ultimo_lucro = vlr
+            ultimo_lucro = l
 
     # se tem muitos lucros descrescentes na lista desc
     if lucros_desc == (ctrl - 1):
