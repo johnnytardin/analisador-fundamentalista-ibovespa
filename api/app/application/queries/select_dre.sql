f8af3165afb29dae78090cd63360d28a263bcb52
Johnny Tardin <johnny.tardin@m4u.com.br>
2020-12-31 13:34:15 -0300

    Atualizando conf

--- api/app/application/queries/select_dre.sql
+++ api/app/application/queries/select_dre.sql
@@ -2,3 +2,7 @@ select
     data
 from dre
 where coleta_id = (SELECT coleta_id FROM dre ORDER BY timestamp DESC LIMIT 1)
+and stock_code not like 'SAPR%%'
+and stock_code not like 'CPLE%%'
+and stock_code not like 'ELET%%'
+and stock_code not like 'CSMG%%'
